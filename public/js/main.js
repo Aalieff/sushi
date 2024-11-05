be0051fbb44f6f8bd4d243cf663ec8f5b469be00
aalieff <aliefanarkis@gmail.com>
2024-11-05 09:52:10 +0800

    rename logo02

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,13 @@ const menu = document.getElementById('menu');
 burgerIcon.addEventListener('click', () => {
   menu.classList.toggle('hidden'); 
 });
+
+//Icon nav
+const icon = ["logo.png", "logo02.png"]
+let index = 0
+const changeImage = ()=>{
+  const img = document.querySelector("#icon")
+  img.src = icon[index]
+  index = (index + 1) % icon.length
+}
+setInterval(changeImage, 5000)
